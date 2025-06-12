0c6f6be2a39c66282265a49c451d90005883f6b4
Advaith K <161608041+advai-007@users.noreply.github.com>
2025-06-12 18:26:18 +0530

    Update app.py

--- app.py
+++ app.py
@@ -2,32 +2,32 @@ import streamlit as st
 import joblib
 import numpy as np
 
-# Load trained model and vectorizer from files
+
 model = joblib.load('spam_model.pkl')
 vectorizer = joblib.load('vectorizer.pkl')
 
-# Title for your web app
-st.title("MessageGuard: SMS Spam Detection")
+
+st.title("SMS Spam Detection")
 
 st.write("""
 This app classifies SMS messages as Spam or Ham using Machine Learning.
 """)
 
-# Text input box
+
 message = st.text_area("Enter your SMS message:", "")
 
-# When user clicks 'Predict'
+
 if st.button("Predict"):
     if message.strip() == "":
         st.warning("Please enter a message.")
     else:
-        # Transform user message into feature vector
+        
         vect_message = vectorizer.transform([message])
         prediction = model.predict(vect_message)[0]
         prob = model.predict_proba(vect_message)[0]
         confidence = np.max(prob) * 100
 
-        # Show result
+        
         if prediction == 1:
             st.error(f"Prediction: Potential SPAM 🚫")
         else:
